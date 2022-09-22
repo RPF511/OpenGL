7419b521ed777e14a46992acfaa1f2197dd3a5fd
RPF511 <sbk98726@gmail.com>
2022-09-22 11:32:52 +0900

    viewport and resize

--- ubuntu/basicPolygon.cpp
+++ ubuntu/basicPolygon.cpp
@@ -4,13 +4,26 @@
 void mydisplay(){
     // glClearColor(0.0, 1.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    
+
     //middle (0,0)
+    glColor3f(1,1,1);
     glBegin(GL_POLYGON);
         glVertex3f(-0.5,-0.5,0.0);
         glVertex3f(0.5,-0.5,0.0);
         glVertex3f(0.5,0.5,0.0);
         glVertex3f(-0.5,0.5,0.0);
     glEnd();
+
+    //dot
+    glColor3f(1,0,0);
+    glPointSize(100);
+    glBegin(GL_POINTS);
+    glVertex2f(0.0,0.0);
+    glEnd();
+
+
     glFlush();
 }
 
